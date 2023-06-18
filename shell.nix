0f40fa74f3911bd6eaf610c60b61c59415333826
Ebrahim Nejati <dearrude@tfwno.gf>
2023-06-19 01:31:42 +0330

    Nixify the project

---
+++ shell.nix
@@ -0,0 +1,25 @@
+{ pkgs ? (let
+  inherit (builtins) fetchTree fromJSON readFile;
+  inherit ((fromJSON (readFile ./flake.lock)).nodes) nixpkgs gomod2nix;
+in import (fetchTree nixpkgs.locked) {
+  overlays = [ (import "${fetchTree gomod2nix.locked}/overlay.nix") ];
+}) }:
+
+let goEnv = pkgs.mkGoEnv { pwd = ./.; };
+in pkgs.mkShell {
+  packages = [
+    # goEnv
+    pkgs.go_1_19
+    pkgs.gomod2nix
+
+    pkgs.gotools
+    pkgs.gofumpt
+    pkgs.golangci-lint
+    pkgs.gopls
+    pkgs.go-outline
+    pkgs.gopkgs
+  ];
+  shellHook = ''
+    export "GOROOT=$(go env GOROOT)"
+  '';
+}
